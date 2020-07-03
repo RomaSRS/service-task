76f7272b4c18da3b3bf1fae0d8d1f53bb4479181
romasrs <shaman.roman@tut.by>
2020-07-03 23:21:57 +0300

    feat: add main block & implement swiper & show content

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 import '../scss/style.scss';
+
+
+import {SwiperBrands} from './swiper-brands';
+import {showContent} from './show-button';
+
+
+SwiperBrands();
+showContent();
---
+++ src/js/swiper-brands.js
@@ -0,0 +1,49 @@
+'use strict';
+
+  import Swiper from 'swiper';
+
+  export const SwiperBrands = function() {
+
+    const newSwiper = document.querySelector('.swiper-container');
+
+  (function() {
+    const breakpoint = window.matchMedia( '(min-width:768px)' );
+    let mySwiper;
+    const breakpointChecker = function() {
+      if (breakpoint.matches === true) {
+        if (mySwiper !== undefined) mySwiper.destroy(true, true);
+          return;
+        } else if (breakpoint.matches === false) {
+          return enableSwiper();
+      }
+    };
+
+    const enableSwiper = function() {
+      mySwiper = new Swiper (newSwiper, {
+        loop: true,
+        loopedSlides: 3,
+        width: 240,
+        spaceBetween: 16,
+        slidesOffsetBefore: 16,
+
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+
+        pagination: {
+          clickable: true,
+          el: '.swiper-pagination',
+        }
+      });
+    };
+
+  // keep an eye on viewport size changes
+    breakpoint.addListener(breakpointChecker);
+
+    // kickstart
+    breakpointChecker();
+
+  })();
+
+  };
